93384b97833f4e86625d3e01ff16984e6c0bfa42
Hilton Bertelli <hilton.bertelli@wisesales.com.br>
2025-09-27 23:09:07 -0300

    Update Supabase anonymous key in config.js
    Updated the Supabase anonymous key in the configuration.

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Configure aqui seu projeto Supabase e a URL base das Functions.
 window.APP_CONFIG = {
   SUPABASE_URL: "https://aeyatfdqglvjdektgudz.supabase.co",
-  SUPABASE_ANON: "sb_secret_-FLTYW2J-i64t37tj1v4eQ_Skh3K2W7",
+  SUPABASE_ANON: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFleWF0ZmRxZ2x2amRla3RndWR6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI3OTAxODAsImV4cCI6MjA2ODM2NjE4MH0.CwOk4VB9S4q00QbkzV3hP5-ckLdSrMHuKqy3VzjyrJk",
   FUNCTIONS_BASE: "https://aeyatfdqglvjdektgudz.supabase.co/functions/v1"
 };
